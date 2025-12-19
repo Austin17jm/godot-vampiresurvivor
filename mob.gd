27e0b3df863be2f66704be617f87ccb46f5a9cc8
austin17jm <austin17jm@hotmail.com>
2025-12-19 06:07:27 -0700

    Added player health, Game Over screen, enemies spawn at random.

--- mob.gd
+++ mob.gd
@@ -5,6 +5,7 @@ var health = 3
 @onready var player = get_node("/root/Game/Player")
 
 func _ready():
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
@@ -13,5 +14,10 @@ func _physics_process(delta: float) -> void:
 
 func take_damage():
 	health -= 1
+	%Slime.play_hurt()
 	if health == 0:
 		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
